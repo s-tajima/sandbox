c184b88c140357860fa250cb9e992a728b52fb09
s-tajima <tajima1989@gmail.com>
2017-10-25 22:56:47 +0900

    improve

--- mysql_replication_different_timezone/mounts/mysql_master/docker-entrypoint-initdb.d/init.sql
+++ mysql_replication_different_timezone/mounts/mysql_master/docker-entrypoint-initdb.d/init.sql
@@ -4,4 +4,3 @@ CREATE USER 'repl'@'%' IDENTIFIED BY 'slavepass';
 GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';
 
 CREATE TABLE time_test ( id INT, dt DATETIME, ts TIMESTAMP );
-INSERT INTO time_test (id, dt, ts) VALUES (1, now(), now()), (2, '2017-10-25 19:05:42', '2017-10-25 19:05:42');
